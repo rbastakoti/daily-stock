5aa5626d4b93212d4b64e8bafdbcd356e711c4b1
Rajan Bastakoti <RBASTAKO@depaul.edu>
2025-01-25 11:07:37 -0600

    added main

--- main.py
+++ main.py
@@ -82,3 +82,7 @@ def get_stock(symbol: str):
     if symbol not in stock_data_cache:
         return {"error": "Stock not found"}
     return {symbol: stock_data_cache[symbol]}
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8080)
